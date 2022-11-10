e4faf7181b0d8d9c58794ee83f0947e1618af0ed
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-11-10 14:31:08 +0000

    Insert data into treasure table

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 const db = require("./");
 
-const format = require("pg-format")
-const seed = ({shopData, treasureData}) => {
-  
+const format = require("pg-format");
+
+const seed = ({ shopData, treasureData }) => {
   return db
     .query(`DROP TABLE IF EXISTS treasures;`)
     .then(() => {
@@ -11,43 +11,65 @@ const seed = ({shopData, treasureData}) => {
     .then(() => {
       return db.query(`CREATE TABLE shops (
 			shop_id SERIAL PRIMARY KEY,
-			shop_name VARCHAR(20) NOT NULL, 
+			shop_name VARCHAR(40) NOT NULL, 
 			owner VARCHAR(40) NOT NULL,
 			slogan TEXT
 		);`);
     })
     .then(() => {
-      console.log("line 20")
       return db.query(`CREATE TABLE treasures (
         treasure_id SERIAL PRIMARY KEY,
-        treasure_name VARCHAR(40),
-        colour VARCHAR(40),
+        treasure_name TEXT,
+        colour TEXT,
         age INT,
         cost_at_auction FLOAT,
+		shop TEXT,
         treasureShop_id INT REFERENCES shops(shop_id)
         );`);
-      })
-      .then(() => {
-        console.log("------------Before formatted shops")
-      const formattedShops = shopData.map((shop) =>{
-        return [
-          shop.shop_name,
-          shop.owner,
-          shop.slogan,
-        ];
+    })
+    .then(() => {
+      const formattedShops = shopData.map((shop) => {
+        return [shop.shop_name, shop.owner, shop.slogan];
       });
-      console.log(formattedShops)
+
       const queryStr = format(
         `INSERT INTO shops
         		(shop_name, owner, slogan)
         		VALUES
         		%L
-        		RETURNING *;`,formattedShops
+        		RETURNING *;`,
+        formattedShops
       );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const formattedTreasures = treasureData.map((treasure) => {
+        return [
+          treasure.treasure_name,
+          treasure.colour,
+          treasure.age,
+          treasure.cost_at_auction,
+          treasure.shop,
+        ];
+      });
+      const queryStr = format(
+        `
+		INSERT INTO treasures (treasure_name,
+		colour,
+		age,
+		cost_at_auction,
+		shop)
+		VALUES %L
+		RETURNING *;`,
+        formattedTreasures
+      );
+      return db.query(queryStr);
+    })
+    .then((data) => {
+      console.log(data.rows);
     });
   // then: create some new tables - but which first and why?
   // then: insert the raw data into the tables.
 };
-
 
 module.exports = seed;
