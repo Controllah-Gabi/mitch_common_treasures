d6d3a7f62af3ac1f34df3368d8477f38421d8347
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-11-10 11:41:56 +0000

    Start mitchs rare treasures

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,43 @@
 const db = require("./");
+const { shopData, treasureData } = require("./data/test-data/index");
 
-const seed = ({ shopData, treasureData }) => {
-	return db.query(`DROP TABLE IF EXISTS treasures;`).then(() => {
-		// drop any existing shops table
-	});
-	// then: create some new tables - but which first and why?
-	// then: insert the raw data into the tables.
+const seed = (shopData, treasureData) => {
+  return db
+    .query(`DROP TABLE IF EXISTS treasures;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS shops`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE shops (
+			shop_id SERIAL PRIMARY KEY,
+			shop_name VARCHAR(20) NOT NULL, 
+			owner VARCHAR(40) NOT NULL,
+			slogan TEXT
+		);`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE treasures (
+			treasure_id SERIAL PRIMARY KEY,
+			treasure_name VARCHAR(40),
+			colour VARCHAR(40),
+			age INT,
+			cost_at_auction FLOAT,
+			shop_id INT REFERENCES shops(shop_id)
+		);`);
+    })
+    .then(() => {
+      console.log(shopData);
+      const queryStr = format(
+        `INSERT INTO shops
+        		(shop_name, owner, slogan)
+        		VALUES
+        		%L
+        		RETURNING *;`
+      );
+    });
+  // then: create some new tables - but which first and why?
+  // then: insert the raw data into the tables.
 };
 
+seed();
 module.exports = seed;
