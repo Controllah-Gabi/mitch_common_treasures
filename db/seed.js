a9ec8bd45fb2d46fe1131f56a5acc588309f82ec
Junior Gabi <juniorgabi@Juniors-MacBook-Air.local>
2022-11-10 14:11:56 +0000

    added the data to the files

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,12 @@
 const db = require("./");
-const { shopData, treasureData } = require("./data/test-data/index");
 
-const seed = (shopData, treasureData) => {
+const format = require("pg-format")
+const seed = ({shopData, treasureData}) => {
+  
   return db
     .query(`DROP TABLE IF EXISTS treasures;`)
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS shops`);
+      return db.query(`DROP TABLE IF EXISTS shops;`);
     })
     .then(() => {
       return db.query(`CREATE TABLE shops (
@@ -16,28 +17,37 @@ const seed = (shopData, treasureData) => {
 		);`);
     })
     .then(() => {
+      console.log("line 20")
       return db.query(`CREATE TABLE treasures (
-			treasure_id SERIAL PRIMARY KEY,
-			treasure_name VARCHAR(40),
-			colour VARCHAR(40),
-			age INT,
-			cost_at_auction FLOAT,
-			shop_id INT REFERENCES shops(shop_id)
-		);`);
-    })
-    .then(() => {
-      console.log(shopData);
+        treasure_id SERIAL PRIMARY KEY,
+        treasure_name VARCHAR(40),
+        colour VARCHAR(40),
+        age INT,
+        cost_at_auction FLOAT,
+        treasureShop_id INT REFERENCES shops(shop_id)
+        );`);
+      })
+      .then(() => {
+        console.log("------------Before formatted shops")
+      const formattedShops = shopData.map((shop) =>{
+        return [
+          shop.shop_name,
+          shop.owner,
+          shop.slogan,
+        ];
+      });
+      console.log(formattedShops)
       const queryStr = format(
         `INSERT INTO shops
         		(shop_name, owner, slogan)
         		VALUES
         		%L
-        		RETURNING *;`
+        		RETURNING *;`,formattedShops
       );
     });
   // then: create some new tables - but which first and why?
   // then: insert the raw data into the tables.
 };
 
-seed();
+
 module.exports = seed;
